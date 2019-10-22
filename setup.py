00904f9082ebef4f6cf131ce63a10dda4addbe48
Paulo José Saiz Jabardo <pjabardo@gmail.com>
2019-10-22 15:49:01 -0300

    Added setup.py file.
    Moved scanivalve.py back to the root. Easier to install!

---
+++ setup.py
@@ -0,0 +1,23 @@
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="scanivalve",
+    py_modules=['scanivalve'],
+    version="0.1",
+    author = "Paulo Jabardo",
+    author_email = "pjabardo@gmail.com",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/tunelipt/scanivalve",
+    packages = setuptools.find_packages(),
+    classifiers = ["Programming Language :: Python :: 3",
+                   "License :: OSI Approved :: MIT License",
+                   "Operating System :: OS Independent"],
+    python_requires='>=3.4')
+
+    
+    
